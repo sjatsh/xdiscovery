51cdd7c24d81c796ec634470623cba453e30ec8f
june <sunjun@qutoutiao.net>
2020-07-20 18:03:10 +0800

    代码结果调整

--- internal/utils/getip.go
+++ internal/utils/getip.go
@@ -14,52 +14,50 @@
 package utils
 
 import (
-    "fmt"
-    "net"
+	"fmt"
+	"net"
 )
 
 type IpType int
 
 const (
-    IpV4 IpType = iota + 1
-    IpV6
+	IpV4 IpType = iota + 1
+	IpV6
 )
 
 // GetLocalIP 获取 eth 网卡的 ip
-func GetLocalIP(eth string,ipType IpType) (string, error) {
-    inters, err := net.Interfaces()
-    if err != nil {
-        return "", err
-    }
+func GetLocalIP(eth string, ipType IpType) (string, error) {
+	inters, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
 
-    for _, inter := range inters {
-        // 检查ip地址判断是否为 eth
-        if inter.Name == eth {
-            addrs, err := inter.Addrs()
-            if err != nil {
-                return "", err
-            }
+	for _, inter := range inters {
+		// 检查ip地址判断是否为 eth
+		if inter.Name == eth {
+			addrs, err := inter.Addrs()
+			if err != nil {
+				return "", err
+			}
 
-            if len(addrs) < 1 {
-                return "", fmt.Errorf("can not found ip")
-            }
+			if len(addrs) < 1 {
+				return "", fmt.Errorf("can not found ip")
+			}
 
-            for _, addr := range addrs {
-                ipNet, _ := addr.(*net.IPNet)
-                switch ipType {
-                case IpV4:
-                    ipv4 := ipNet.IP.To4()
-                    if ipv4 != nil {
-                        return ipv4.String(), nil
-                    }
-                case IpV6:
-                    ipv6 := ipNet.IP.To16()
-                    if ipv6 != nil {
-                        return ipv6.String(), nil
-                    }
-                }
-            }
-        }
-    }
-    return "", fmt.Errorf("can not found %s", eth)
+			for _, addr := range addrs {
+				ipNet, _ := addr.(*net.IPNet)
+				switch ipType {
+				case IpV4:
+					if ipv4 := ipNet.IP.To4(); ipv4 != nil {
+						return ipv4.String(), nil
+					}
+				case IpV6:
+					if ipv6 := ipNet.IP.To16(); ipv6 != nil {
+						return ipv6.String(), nil
+					}
+				}
+			}
+		}
+	}
+	return "", fmt.Errorf("can not found %s", eth)
 }
